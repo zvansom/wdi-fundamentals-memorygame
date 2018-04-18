e2b0683faf32081d71cf5704bb6d4a26ea092aae
Zach VanSomeren <zvansomeren@gmail.com>
2018-04-18 17:27:48 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,33 @@
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = [
+  {
+  rank: 'queen',
+  suit: 'hearts',
+  cardImage: 'images/queen-of-hearts.png',
+  },
+  {
+    rank: 'queen',
+    suit: 'diamonds',
+    cardImage: 'images/queen-of-diamonds.png',
+  },
+  {
+    rank: 'king',
+    suit: 'hearts',
+    cardImage: 'images/king-of-hearts.png',
+  },
+  {
+    rank: 'king',
+    suit: 'hearts',
+    cardImage: 'images/king-of-diamonds.png',
+  }
+];
 var cardsInPlay = [];
+
 var flipCard = function(cardId) {
-  console.log("User flipped " + cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
+  console.log("User flipped " + cards[cardId].rank);
+  cardsInPlay.push(cards[cardId].rank);
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
+
   checkForMatch(cardsInPlay[0], cardsInPlay[1])
 }
 
