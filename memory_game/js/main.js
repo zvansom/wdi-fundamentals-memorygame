da793c8ae97788a26128a91e4badf96aa8a1eb05
Zach VanSomeren <zvansomeren@gmail.com>
2018-04-18 16:25:35 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,17 +1,20 @@
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
+var flipCard = function(cardId) {
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+  checkForMatch(cardsInPlay[0], cardsInPlay[1])
+}
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardsInPlay[0]);
-console.log("User flipped " + cardsInPlay[1]);
-
-if (cardsInPlay.length >= 2) {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
-  } else {
-    alert("Sorry, try again.");
+var checkForMatch = function(card1, card2) {
+  if (cardsInPlay.length >= 2) {
+    if (card1 === card2) {
+      alert("You found a match!");
+    } else {
+      alert("Sorry, try again.");
+    }
   }
 }
+
+flipCard(0);
+flipCard(2);
